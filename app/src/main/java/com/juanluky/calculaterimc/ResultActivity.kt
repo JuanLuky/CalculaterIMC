ce0ba45a967e55fcec28f60f5ea02e263c7babca
JuanLuky <juandev33@gmail.com>
2023-05-31 15:54:52 -0300

    neCalculaterIMCOstyleCriação das interfaces do app

---
+++ app/src/main/java/com/juanluky/calculaterimc/ResultActivity.kt
@@ -0,0 +1,16 @@
+package com.juanluky.calculaterimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juanluky.calculaterimc.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
