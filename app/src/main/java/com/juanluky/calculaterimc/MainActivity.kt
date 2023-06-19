0c1973dd2074cbab82dd980a5e6abab4cfeb5140
Juan Carlos <juandev33@gmail.com>
2023-06-19 15:48:49 -0300

    Resu(ltActivity() A)acorrigindo nas condiçÃÃões!

--- app/src/main/java/com/juanluky/calculaterimc/MainActivity.kt
+++ app/src/main/java/com/juanluky/calculaterimc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.juanluky.calculaterimc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.juanluky.calculaterimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +16,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.btnCalcular.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            startActivity(intent)
+//      Caixa de alerta
+        fun onBackPressed() {
+            val builder = AlertDialog.Builder(this)
+
+            builder.setMessage("Preecha os campos vazios.")
+
+            builder.setTitle("Alerta !")
+
+            builder.setCancelable(false)
+
+            builder.setPositiveButton("OK") {
+                    dialog, which -> dialog.cancel()
+            }
+            val alertDialog = builder.create()
+            alertDialog.show()
         }
+
+        val peso = binding.inputPesoValue
+        val altura = binding.inputAlturaValue
+
+        binding.btnCalcular.setOnClickListener {
+            if(peso.text.isNullOrEmpty() || altura.text.isNullOrEmpty()){
+                onBackPressed()
+            } else {
+                val intent = Intent(this, ResultActivity::class.java)
+
+                intent.putExtra("peso", peso.text.toString())
+                intent.putExtra("altura", altura.text.toString())
+
+                startActivity(intent)
+                peso.setText("")
+                altura.setText("")
+            }
+        }
+
     }
 }
--- app/src/main/java/com/juanluky/calculaterimc/ResultActivity.kt
+++ app/src/main/java/com/juanluky/calculaterimc/ResultActivity.kt
@@ -1,5 +1,6 @@
 package com.juanluky.calculaterimc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.juanluky.calculaterimc.databinding.ActivityResultBinding
@@ -8,9 +9,41 @@ class ResultActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityResultBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val dados = intent.extras
+
+        val peso = dados?.getString("peso")
+        val altura = dados?.getString("altura")
+
+        binding.pesoInformado.text = "Peso informado: $peso"
+        binding.alturaInformada.text = "Altura informada: $altura"
+
+
+        //Diagnóstico
+
+        val pesoNum = peso!!.toDouble()
+        val alturaNum = altura!!.toDouble()
+
+        val calc = pesoNum / (alturaNum * alturaNum)
+//      Format number two houses decimals
+
+        if (calc <= 18.6) {
+            binding.resultado.text = "BAIXO"
+        }else if (calc in 18.6..24.9) {
+            binding.resultado.text = "NORMAL"
+        } else if (calc in 25.0..29.9){
+            binding.resultado.text = "SOBREPESO"
+        } else {
+            binding.resultado.text = "OBESIDADE"
+        }
+
     }
 }
+
+
